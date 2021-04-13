f235e094cabb3a19fb5009bced5786058d966823
Poellie <52569709+Poellie01@users.noreply.github.com>
2021-04-13 14:25:40 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,4 +1,158 @@
 #!/bin/bash
+
+menu_option_one() {
+        echo "Checking with yum for updates"
+        yumtmp="/tmp/yum-check-update.$$"
+        yum="/usr/bin/yum"
+        $yum check-update >& $yumtmp
+        yumstatus="$?"
+        hostname=$(/bin/hostname)
+        case $yumstatus in
+        o)
+        echo "No Updates"
+        exit o
+        ;;
+        *)
+        date=$(date)
+        number=$(cat $yumtmp | egrep '(.i386|.x86_64|.noarch|.src)'| wc -l)
+        updates=$(cat $yumtmp | egrep '(.i386|.x86_64|.noarch|.src)')
+        echo "There are $number updates available on host $hostname at $date"
+        echo "The available updates are"
+        echo ""
+        echo "$updates"
+        ;;
+        esac
+        rm -f /tmp/yum-check-update.*
+}
+menu_option_two() {
+        echo "Host machine INFO"
+        echo ""
+        echo "          1 - Display Basic information"
+        echo "          2 - Display Full information"
+        echo "          3 - Display CPU information"
+        echo "          4 - Display GPU information"
+        echo ""
+        echo -n "Enter Selection: "
+        read selection
+        echo ""
+        case $selection in
+        1) clear ; menu_option_two_basic ;;
+        2) clear ; menu_option_two_full ;;
+        3) clear ; menu_option_two_cpu ;;
+        4) clear ; menu_option_two_gpu ;;
+        *) clear ; incorrect_selection ; press_enter ;;
+        esac
+}
+menu_option_two_basic() {
+        hostname=$(/bin/hostname)
+        date=$(date)
+        echo "Basic Host /  Kernel / IP / Disk information "
+        echo ""
+        uname -a
+        echo ""
+        echo "Current TCP/IP configuration: "
+        echo ""
+        if ! command -v ifconfig &> /dev/null
+        then
+                ip link show
+        else
+                ifconfig
+        fi
+        echo ""
+        echo "Hard disks"
+        echo ""
+        lsblk
+        echo ""
+        echo "          1. Save output to file"
+        echo "          2. Return to main menu"
+        echo ""
+        echo "Select action: "
+        read action
+        echo ""
+        case $action in
+        1) basic_output_to_file ;;
+        2) clear ;;
+        *) incorrect_selection ; press_enter ;;
+        esac
+}
+menu_option_two_full() {
+        echo "Full list information"
+        lshw
+        echo ""
+        echo "          1. Save output to file"
+        echo "          2. Return to main menu"
+        echo ""
+        echo "Select action: "
+        read action
+        echo ""
+        case $action in
+        1) full_save_output_to_file ;;
+        2) clear ;;
+        *) incorrect_selection ; press_enter ;;
+        esac
+}
+menu_option_two_cpu() {
+        echo "CPU information"
+        echo ""
+        lscpu
+        echo ""
+        echo ""
+        echo "          1. Save Output to file"
+        echo "          2. Return to main menu"
+        echo ""
+        echo "Select action: "
+        read action
+        echo ""
+        case $action in
+        1) cpu_save_output_to_file ;;
+        2) clear ;;
+        *) incorrect_selection ; press_enter ;;
+        esac
+}
+menu_option_two_gpu() {
+        echo ""
+        echo "Basic GPU information"
+        echo ""
+        lshw -C display
+        echo ""
+        echo "          1. Save output to file"
+        echo "          2. Return to main menu"
+        echo ""
+        echo "Select action: "
+        read action
+        echo ""
+        case $action in
+        1) gpu_save_output_to_file ;;
+        2) clear ;;
+        *) incorrect_selection ;;
+        esac
+}
+menu_option_three() {
+        echo "Logged in users:"
+        echo ""
+        w
+        echo ""
+        echo "          1. List all users"
+        echo "          2. Return to main menu"
+        echo ""
+        echo "Select action: "
+        read action
+        case $action in
+        1) list_all_users ;;
+        2) clear ;;
+        *) incorrect_selection ;;
+        esac
+}
+menu_option_four() {
+        echo ""
+        echo "Network stats"
+        echo ""
+        netstat -i
+}
+menu_option_five() {
+        echo "Press CTLR + C to get back to the main menu"
+        top
+
 }
 menu_option_six() {
         echo""
