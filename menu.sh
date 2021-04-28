f83f9b1a678a1434057874cf0c7b8af1478bb9d2
Poellie <52569709+Poellie01@users.noreply.github.com>
2021-04-28 10:42:28 +0200

    Update menu.sh
    Changed some function names, and added more arrays and strings

--- menu.sh
+++ menu.sh
@@ -31,6 +31,7 @@ menu_option_two() {
         echo "          2 - Display Full information"
         echo "          3 - Display CPU information"
         echo "          4 - Display GPU information"
+        echo "          0 - Return to main menu"
         echo ""
         echo -n "Enter Selection: "
         read selection
@@ -41,6 +42,7 @@ menu_option_two() {
         3) clear ; menu_option_two_cpu ;;
         4) clear ; menu_option_two_gpu ;;
         *) clear ; incorrect_selection ; press_enter ;;
+        0) clear ;;
         esac
 }
 menu_option_two_basic() {
@@ -133,13 +135,15 @@ menu_option_three() {
         w
         echo ""
         echo "          1. List all users"
-        echo "          2. Return to main menu"
+        echo "          2. List users cli history"
+        echo "          3. Return to main menu"
         echo ""
         echo "Select action: "
         read action
         case $action in
         1) list_all_users ;;
-        2) clear ;;
+        2) user_history ;;
+        3) clear ;;
         *) incorrect_selection ;;
         esac
 }
@@ -148,15 +152,42 @@ menu_option_four() {
         echo "Network stats"
         echo ""
         netstat -i
+        echo ""
+        ifconfig
 }
 menu_option_five() {
-        echo "Press CTLR + C to get back to the main menu"
         top
-
 }
 menu_option_six() {
         echo""
         history
+}
+menu_option_seven() {
+        echo ""
+        echo "          1. Check all auth logs"
+        echo "          2. Check last 50 entries"
+        echo "          3. Exit to main menu"
+        echo ""
+        echo "Select action: "
+        read action
+        case $action in
+        1) auth_log_full ;;
+        2) auth_log_entries ;;
+        3) clear ;;
+        esac
+}
+menu_option_ten() {
+        echo ""
+        echo "Listing all known hosts"
+        echo ""
+        cat /etc/hosts
+}
+auth_log_full() {
+        echo ""
+        cat /var/log/secure
+}
+auth_log_entries() {
+        tail -50 /var/log/secure
 }
 list_all_users() {
         echo ""
@@ -165,65 +196,72 @@ list_all_users() {
 }
 user_history() {
         echo ""
-        user1="Dylan Poelstra"
-        user2="Eduard Regenboog"
-        user3="Theo Kastermans"
-        echo "History of user $user1"
-        echo ""
-        cat /home/a-poelstd/.bash_history
-        echo ""
-        echo "History of user $user2"
-        cat /home/a-eregenboog/.bash_history
-        echo ""
-        echo "History of user $user3"
-        cat /home/a-tkastermans/.bash_history
-        echo ""
         echo "          1. Save history of specific user"
-        echo "          2. Return to main menu"
+        echo "          2. List last 50 commands from all users"
+        echo "          3. List all commands of all users"
+        echo "          4. Exit to main menu"
         echo ""
         echo "Select action: "
         read action
         case $action in
         1) save_user_history ;;
-        2) clear ;;
+        2) user_history_part ;;
+        3) user_history_all ;;
+        4) clear ;;
         esac
+}
+user_history_all() {
+        echo ""
+        cat /var/log/cmd.log
+}
+user_history_part()  {
+        echo ""
+        tail -50 /var/log/cmd.log
 }
 save_user_history(){
         echo ""
         echo "Which users history would you like to save?"
-        echo "1. Dylan Poelstra"
-        echo "2. Theo Kastermans"
-        echo "3. Eduard Regenboog"
+        echo "1. User1"
+        echo "2. User2"
+        echo "3. User3"
         echo "4. root"
+        echo "5. All users"
         echo ""
         echo "Select user: "
         read action
         case $action in
-        1) save_user_history_poelstra ;;
-        2) save_user_history_kastermans ;;
-        3) save_user_history_regenboog ;;
+        1) save_user_history_user1 ;;
+        2) save_user_history_user2 ;;
+        3) save_user_history_user3 ;;
         4) save_user_history_root ;;
+        5) save_user_history_all ;;
+        *) clear ; incorrect_selection ;;
         esac
 }
-save_user_history_poelstra() {
-        cat /home/a-poelstd/.bash_history >> /tmp/PoelstraHistory.txt
+save_user_history_user1() {
+        cat /home/username/.bash_history >> /tmp/PoelstraHistory.txt
         echo ""
-        echo "Saved as /tmp/PoelstraHistory.txt"
+        echo "Saved as /tmp/User1History.txt"
 }
-save_user_history_kastermans() {
-        cat /home/a-tkastermans/.bash_history >> /tmp/KastermansHistory.txt
+save_user_history_user2() {
+        cat /home/username/.bash_history >> /tmp/KastermansHistory.txt
         echo ""
-        echo "Saved as /tmp/KastermansHistory.txt"
+        echo "Saved as /tmp/User1History.txt"
 }
-save_user_history_regenboog() {
-        cat /home/a-eregenboog/.bash_history >> /tmp/RegenboogHistory.txt
+save_user_history_user3() {
+        cat /home/username/.bash_history >> /tmp/RegenboogHistory.txt
         echo ""
-        echo "Saved as /tmp/RegenboogHistory.txt"
+        echo "Saved as /tmp/User2History.txt"
 }
 save_user_history_root() {
         cat /root/.bash_history >> /tmp/RootHistory.txt
         echo ""
         echo "Saved as /tmp/root.txt"
+}
+save_user_history_all() {
+        cat /home/user/.bash_history && cat /home/user1/.bash_history && cat /home/user2/.bash_history && cat /root/.bash_history >> /tmp/AllUsersHistory.txt
+        echo ""
+        echo "Saved as /tmp/AllUsersHistory.txt"
 }
 basic_output_to_file() {
         uname -a && ifconfig && lsblk >> /tmp/BasicInfo.txt &> /dev/null
@@ -258,9 +296,11 @@ until ["$selection" = "0" ]; do
         then
                 COLUMNS=$(tput cols)
                 title=$(date)
+                author="Dylan Poelstra"
                 banner="Banner"
                 printf "%*s\n" $(((${#banner}+$COLUMNS)/2)) "$banner"
                 printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+                printf "%*s\n" $(((${#author}+$COLUMNS)/2)) "$author"
         else
                 echo "Banner" | figlet -ctk
                 COLUMNS=$(tput cols)
@@ -270,9 +310,9 @@ until ["$selection" = "0" ]; do
                 printf "%*s\n" $(((${#author}+$COLUMNS)/2)) "$author"
         fi
         echo ""
-        echo "          1 - Check for updates with yum                          5- List current running processes"
-        echo "          2 - Host Information                                    6- Command history"
-        echo "          3 - Currently logged in users / History                 7- Test "
+        echo "          1 - Check for updates with yum                          5- List current running processes               10. List known hosts"
+        echo "          2 - Host Information                                    6- Command History"
+        echo "          3 - Currently logged in users / History                 7- View authentication log"
         echo "          4 - Network stats                                       8- Test "
         echo "          0 - Exit                                                9- Test "
         echo ""
@@ -285,7 +325,11 @@ until ["$selection" = "0" ]; do
         3) clear ; menu_option_three ; press_enter ;;
         4) clear ; menu_option_four ; press_enter ;;
         5) clear ; menu_option_five ; press_enter ;;
-        6) clear ; menu_option_six ; press_enter ;;
+        6) clear ; user_history ; press_enter ;;
+        7) clear ; menu_option_seven ; press_enter ;;
+        8) echo "Not available yet" ;;
+        9) echo "Not available yet" ;;
+        10) clear ; menu_option_ten ; press_enter ;;
         0) clear ; exit ;;
         *) clear ; incorrect_selection ; press_enter ;;
         esac
